d5cfc930845425bacf3d1e4ea501944e7d4520df
c0natus <hks8946@gmail.com>
2021-12-06 13:13:01 +0900

    동전 1

--- Algorithm-practice/백준/실버 이하/B2293.py
+++ Algorithm-practice/백준/실버 이하/B2293.py
@@ -8,10 +8,13 @@ for i in range(n):
 # dp[i] = 합이 i가 될 수 있는 경우의 수
 # dp[i] = (dp[1] + dp[i-1]) + (dp[2] + dp[i-2]) + ...
 dp = [0] * (k+1)
+
 dp[0] = 1
 
 for i in range(n):
+    # coin[i]로만 만들 수 있는 합의 경우의 수를 dp에 누적해서 더한다.
     for j in range(coin[i], k+1):
+        # coin[i] == j일 때, 경우의 수는 1이므로 dp[0] = 1이 되어야
         dp[j] += dp[j-coin[i]]
 
 print(dp[k])
