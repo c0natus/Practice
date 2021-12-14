57b6e7e277540eb54cfca49ab59dfce2b0afbec3
c0natus <hks8946@gmail.com>
2021-12-14 16:24:24 +0900

    미로탐색

--- Algorithm-practice/백준/실버 이하/B2178.py
+++ Algorithm-practice/백준/실버 이하/B2178.py
@@ -1,7 +1,31 @@
 """
 별로 어렵지 않게 bfs 풀었다.
-
-메모리를 아끼는 관점에서 다른 사람의 풀이를 참조하였다.
-처음에는 is_visited라는 배열과 count라는 값을 따로 저장했지만
-이것을 table로 합쳐서 표현할 수 있다.
 """
+
+from collections import deque
+
+n, m = map(int, input().split())
+table = [input() for _ in range(n)]
+is_visited = [[False] * m for _ in range(n)]
+
+# 상, 하 좌, 우
+move_x = [-1, 1, 0, 0]
+move_y = [0, 0, -1, 1]
+
+q = deque([[0, 0, 1]])
+while True:
+    row, col, count = q.popleft()
+
+    if row == n - 1 and col == m - 1:
+        print(count)
+        break
+
+    tmp_count = count + 1
+    for idx in range(4):
+        tmp_row = row + move_x[idx]
+        tmp_col = col + move_y[idx]
+
+
+        if 0 <= tmp_row < n and 0 <= tmp_col < m and is_visited[tmp_row][tmp_col] is False and table[tmp_row][tmp_col] == '1':
+            q.append([tmp_row, tmp_col, tmp_count])
+            is_visited[tmp_row][tmp_col] = True
