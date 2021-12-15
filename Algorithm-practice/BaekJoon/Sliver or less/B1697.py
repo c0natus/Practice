78ebeaacdcbc612644645fb67a468fced3c02400
c0natus <hks8946@gmail.com>
2021-12-15 15:11:32 +0900

    숨바꼭질 최적화

--- Algorithm-practice/BaekJoon/Sliver or less/B1697.py
+++ Algorithm-practice/BaekJoon/Sliver or less/B1697.py
@@ -1,52 +1,22 @@
-from collections import deque
+"""
+좀더 생각해서 예외 처리를 해서 시간을 줄이자
+reference: https://www.acmicpc.net/source/23747535
+"""
 
-def bfs(n, k):
-    if n == k:
-        return 0
-        
-    is_visited[n] = True
-    q = deque([n])
-    sec = 0
+def find(n, k):
+    if n >= k:
+        # 뒤로 가는 경우는 -1 이동만 있기 때문이다.
+        return n - k
+    elif k == 1:
+        # n이 0이고 k가 1인 경우 *2의 움직임보단 +1 움직임으로 찾는다.
+        return 1
+    elif k % 2 == 0:
+        # k가 짝수인 경우 k를 n만큼 뒤로 이동한 경우와
+        # 2로 나눈 후 찾은 이동 수 중 더 작은 값을 반환한다.
+        return min(k-n, find(n, k//2) + 1)
+    else:
+        # k가 홀수인 경우 앞, 뒤로 1씩 이동한 후
+        # 이동 경우의 수 중 작은 값을 반환한다.
+        return min(find(n, k-1), find(n, k+1)) + 1
 
-    is_hit = False
-
-    while q:
-        tmp_q = deque()
-        sec += 1
-
-        while q:
-            position = q.popleft()
-
-            next_pos = position - 1
-            if next_pos == k:
-                is_hit = True
-                break
-            if next_pos >= 0 and next_pos <= 100000 and is_visited[next_pos] is False:
-                is_visited[next_pos] = True
-                tmp_q.append(next_pos)
-            
-            next_pos = position + 1
-            if next_pos == k:
-                is_hit = True
-                break
-            if next_pos >= 0 and next_pos <= 100000 and is_visited[next_pos] is False:
-                is_visited[next_pos] = True
-                tmp_q.append(next_pos)
-            
-            next_pos = position * 2
-            if next_pos == k:
-                is_hit = True
-                break
-            if next_pos >= 0 and next_pos <= 100000 and is_visited[next_pos] is False:
-                is_visited[next_pos] = True
-                tmp_q.append(next_pos)
-        
-        if is_hit:
-            break
-        q = tmp_q
-
-    return sec
-
-INF = 100005
-is_visited = [False] * INF
-print(bfs(*map(int, input().split())))
+print(find(*map(int, input().split())))
