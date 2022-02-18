ab0d38e09be9d6bc6a4c7aa5ee2028803c196c3d
c0natus <hks8946@gmail.com>
2022-02-19 00:36:08 +0900

    공유기 설치

--- Algorithm-practice/BaekJoon/Gold or higher/B2110.py
+++ Algorithm-practice/BaekJoon/Gold or higher/B2110.py
@@ -8,6 +8,10 @@ distance_high = house_position[-1] - house_position[0] + 1 # house_position[-1]
 
 answer = 0
 
+# distance = (low+high)//2
+# distance를 기준으로 distance보다 간격이 넓은 곳에만 공유기를 설치한다.
+# 만약 공유기를 모두 설치한다면, distance_low를 distance+1로 높인다.
+# 만약 공유기를 모두 설치할 수 없다면, distance_high의 값을 distance-1로 낮춘다.
 while distance_low <= distance_high:
     distance = (distance_high+distance_low) // 2
 
